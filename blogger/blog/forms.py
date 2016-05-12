90e4eb073ee8946379e34a00fcccdaa3a75e90a9
Ryan Stack <ryan@gravity4.com>
2016-05-12 09:01:25 -0700

    Added bootstrap buttons and form styling

--- blogger/blog/forms.py
+++ blogger/blog/forms.py
@@ -1,13 +1,13 @@
 from django import forms
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea
 
 from .models import Post
-
-# class PostForm(forms.Form):
-#     title = forms.CharField(label='title', max_length=50)
-#     description = forms.CharField(label='description', max_length=200)
 
 class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ["title", "description"] 
+        widgets = {
+            'title': Textarea(attrs={'cols': 80, 'rows': 20}),
+            'description': Textarea(attrs={'cols': 80, 'rows': 20}),
+        }
