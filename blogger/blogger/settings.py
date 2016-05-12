168d8e71fc4d8e05734fa207b2826bd8e6f09a61
Ryan Stack <ryan@gravity4.com>
2016-05-11 17:10:41 -0700

    removed remaining practice code

--- blogger/blogger/settings.py
+++ blogger/blogger/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
-    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
